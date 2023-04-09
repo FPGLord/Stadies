9563b90531cd169bd9f7824cfc223f2f28cd61cb
Lord <Lord@DESKTOP-ACS660U>
2023-04-09 20:41:08 +0300

    Добавьте файлы проекта.

---
+++ AppProg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppProg
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int age = 40;
+            string myName = "Дмитрий";
+            double distance = 122.4;
+            bool openDoor = false;
+            float speed = 10.6f;
+            char pressKeyY = 'Y';
+            short houseNumber = 84;
+            uint roomNumber = 3;
+            decimal passedDistance = 22344.4M;
+            long distanceToNextEnemy = 234;
+
+
+        }
+    }
+}
+
+   
