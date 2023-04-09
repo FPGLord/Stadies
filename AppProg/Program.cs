9334e00c2f4a0d968126b582cc5a8e09c42137ff
FPGLord <127301692+FPGLord@users.noreply.github.com>
2023-04-09 22:34:31 +0300

    Update Program.cs

--- AppProg/Program.cs
+++ AppProg/Program.cs
@@ -4,21 +4,18 @@ namespace AppProg
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             int age = 40;
-            string myName = "Дмитрий";
+            string streetName = "Ленина";
             double distance = 122.4;
-            bool openDoor = false;
+            bool isGrounded = false;
             float speed = 10.6f;
-            char pressKeyY = 'Y';
+            char jumpButton = 'Y';
             short houseNumber = 84;
             uint roomNumber = 3;
             decimal passedDistance = 22344.4M;
             long distanceToNextEnemy = 234;
-
-
         }
     }
 }
